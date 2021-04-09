2e60a11a2a69eacafa1372dcbc55b9db73aa4e58
Rainer Simon <aboutgeo@gmail.com>
2021-04-09 10:54:43 +0200

    Minor tweaks

--- packages/widget-react-helloworld/src/index.js
+++ packages/widget-react-helloworld/src/index.js
@@ -3,13 +3,10 @@ import React from 'preact/compat';
 import './index.css';
 
 const HelloWorldWidget = props => {
-
-  const { annotation } = props;
-
   // We'll be using 'highlighting' as body purpose for 
   // this type of widget
-  const currentHighlight = annotation ? 
-    annotation.bodies.find(b => b.purpose === 'highlighting') : null;
+  const currentHighlight = props.annotation ? 
+    props.annotation.bodies.find(b => b.purpose === 'highlighting') : null;
 
   // This function handles body updates as the user presses buttons
   const setHighlightBody = value => () => {
@@ -19,20 +16,12 @@ const HelloWorldWidget = props => {
   return (
     <div className="helloworld-widget">
       { [ 'red', 'green', 'blue' ].map(color => 
-
         <button 
-          key={color}
-    
-          // Set class to 'selected' if this button value equals current highlight
+          key={color}    
           className={currentHighlight?.value === color ? 'selected' : null}
-
-          // Set color value as CSS background
           style={{ backgroundColor: color }}
-
-          // On click add (or replace) the current highlight body
           onClick={setHighlightBody(color)} />
-
-    )}
+      )}
     </div>
   )
 
