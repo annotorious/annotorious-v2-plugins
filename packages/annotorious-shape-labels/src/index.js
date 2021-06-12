c2867a4bebb8d060e95d923d849f39be018eaf87
Rainer Simon <aboutgeo@gmail.com>
2021-06-12 13:53:19 +0200

    Bugfixes

--- packages/annotorious-shape-labels/src/index.js
+++ packages/annotorious-shape-labels/src/index.js
@@ -10,15 +10,18 @@ const ShapeLabelsFormatter = config => annotation => {
   if (firstTag) {
     const foreignObject = document.createElementNS('http://www.w3.org/2000/svg', 'foreignObject');
 
-    // Fill annotation dimensions
-    foreignObject.setAttribute('width', '100%');
-    foreignObject.setAttribute('height', '100%');
+    // Overflow is set to visible, but the foreignObject needs >0 zero size,
+    // otherwise FF doesn't render...
+    foreignObject.setAttribute('width', '1px');
+    foreignObject.setAttribute('height', '1px');
 
     foreignObject.innerHTML = `
-      <div xmlns="http://www.w3.org/1999/xhtml" class="a9s-shape-label">
-        ${firstTag.value}
+      <div xmlns="http://www.w3.org/1999/xhtml" class="a9s-shape-label-wrapper">
+        <div class="a9s-shape-label">
+          ${firstTag.value}
+        </div>
       </div>`;
-
+      
     return {
       element: foreignObject,
       className: firstTag.value
