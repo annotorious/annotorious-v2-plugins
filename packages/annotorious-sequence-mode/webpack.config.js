e2bd8597d90160904802d56af588be46739aeee7
Rainer Simon <aboutgeo@gmail.com>
2021-06-22 07:41:21 +0200

    Project skeleton: sequence mode plugin

---
+++ packages/annotorious-sequence-mode/webpack.config.js
@@ -0,0 +1,65 @@
+const path = require('path');
+const fs = require('fs');
+
+const APP_DIR = fs.realpathSync(process.cwd());
+
+const resolveAppPath = relativePath => path.resolve(APP_DIR, relativePath);
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+
+module.exports = {
+  entry: resolveAppPath('src'),
+  output: {
+    filename: 'annotorious-sequence-mode.min.js',
+    library: ['OpenSeadragon', 'AnnotoriousSequenceMode'],
+    libraryTarget: 'umd',
+    libraryExport: 'default',
+    pathinfo: true
+  },
+  performance: {
+    hints: false
+  },
+  devtool: 'source-map',
+  optimization: {
+    minimize: true,
+    minimizer: [ new TerserPlugin() ]
+  },
+  resolve: {
+    extensions: ['.js' ]
+  },
+  module: {
+    rules: [
+      { 
+        test: /\.js$/, 
+        use: { 
+          loader: 'babel-loader' ,
+          options: {
+            "presets": [
+              "@babel/preset-env"
+            ],
+            "plugins": [
+              [
+                "@babel/plugin-proposal-class-properties"
+              ]
+            ]
+          }
+        }
+      },
+      { test: /\.scss$/, use: [ 'style-loader', 'css-loader', 'sass-loader' ] }
+    ]
+  },  
+  devServer: {
+    contentBase: [ resolveAppPath('public'), resolveAppPath('../../public') ],
+    compress: true,
+    hot: true,
+    host: process.env.HOST || 'localhost',
+    port: 3000,
+    publicPath: '/'
+  },
+  plugins: [
+    new HtmlWebpackPlugin ({
+      template: resolveAppPath('public/index.html')
+    })
+  ]
+}
