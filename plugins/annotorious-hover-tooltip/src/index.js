b6c84969bf5308e0aae90684aecff9df35d01f92
Rainer Simon <aboutgeo@gmail.com>
2022-01-23 10:25:00 +0100

    First working hack

--- plugins/annotorious-hover-tooltip/src/index.js
+++ plugins/annotorious-hover-tooltip/src/index.js
@@ -1,3 +1,44 @@
+import './index.css';
+
+const getFirstText = annotation => {
+  
+}
+
 export default anno => {
-  console.log('TODO...');
+
+  let tooltip = null;
+
+  const onMouseMove = evt => {
+    tooltip.style.top = `${evt.clientY}px`;
+    tooltip.style.left = `${evt.clientX}px`;
+  }
+
+  const showTooltip = (annotation, shape) => {
+    // Create tooltip element
+    tooltip = document.createElement('div');
+    tooltip.setAttribute('class', 'a9s-hover-tooltip');
+
+    // TODO get first TextualBody
+    tooltip.innerHTML = 'Hello World!';
+
+    anno._element.appendChild(tooltip);
+
+    shape.addEventListener('mousemove', onMouseMove);
+  }
+
+  const hideTooltip = (annotation, shape) => {
+    shape.removeEventListener('mousemove', onMouseMove);
+
+    if (tooltip) {
+      anno._element.removeChild(tooltip);
+      tooltip = null;
+    }
+  }
+
+  anno.on('mouseEnterAnnotation', (annotation, shape) =>
+    showTooltip(annotation, shape));
+
+  anno.on('mouseLeaveAnnotation', (annotation, shape) =>
+    hideTooltip(annotation, shape));
+
 }
